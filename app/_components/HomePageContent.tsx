969b2bceb4c792107b4dcfeb2451d8ed6751f613
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-21 11:38:13 +0300

    loggedInUser issue

---
+++ app/_components/HomePageContent.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+function HomePageContent() {
+  return (
+    <div>
+      <p style={{ fontSize: "50px" }}>HomePageContent</p>
+    </div>
+  );
+}
+export default HomePageContent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 // app/page.tsx
-
 import { Metadata } from 'next';
-import ClientComponent from '../app/_components/ClientComponent';
-
-
+//import HomePageContent from './HomePageContent';
+//import { getUsers, getTeams } from '../services/apiGroups';
+import HomePageContent from './_components/HomePageContent';
+import { getTeams, getUsers } from './_services/apiGroups';
 
 export const metadata: Metadata = {
   title: 'Home',
   description: 'Welcome to chatSPA',
 };
 
-export default function HomePage() {
+export default async function HomePage() {
+  const users = await getUsers();
+  const teams = await getTeams(+loggedInUser.id); // Replace 1 with appropriate user ID
+
+  // Simulate fetching logged-in user and other state details
+  const loggedInUser = { id: 1, name: 'User' };
+  const isLoading = false;
+  const isRegister = false;
+
   return (
     <div className="app-container" style={{ position: 'relative' }}>
-      <ClientComponent />
+      <HomePageContent 
+        loggedInUser={loggedInUser}
+        teams={teams}
+        users={users}
+        isLoading={isLoading}
+        isRegister={isRegister}
+      />
     </div>
   );
 }
