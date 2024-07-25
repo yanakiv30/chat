6e47de4ef30b25e3f38c99533b50f9709cc6f9ef
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-25 16:30:52 +0300

    getUsers()

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,13 +1,29 @@
 import { auth } from "../_services/auth";
+import { supabase } from "../_services/supabase";
 
 async function page() {
   const session = await auth();
-  console.log("session= ", session);
+  console.log("session= ", session); 
+
+   async function getUsers() {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")     
+    console.log("data from getUsers = ",data);
+    
+    if (error) {
+      console.error(error);      
+    }  
+    return JSON.stringify(data);
+  }
+let result= await getUsers()
+
   return (
     <div>
       <p style={{ fontSize: "50px" }}>
         This account is the result of signin Google provider
       </p>
+      {result}
     </div>
   );
 }
