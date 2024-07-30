39511623a3f561af3112e434222de6bbb742ce33
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-30 19:24:16 +0300

    Sign with Google button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-
 import { Metadata } from "next";
-import Link from 'next/link';
+import Link from "next/link";
 export const metadata: Metadata = {
   title: "Home",
   description: "Welcome to chatSPA",
@@ -8,13 +7,27 @@ export const metadata: Metadata = {
 export default async function HomePage() {
   return (
     <div className="app-container" style={{ position: "relative" }}>
-      <p style={{fontSize:"50px"}}>Welcome to Chat</p>
-      <Link href="/login">
-        <button >
-          Go to Login
-        </button>
-      </Link>
-      
+      <div>
+        <p style={{ fontSize: "70px" }}>Welcome to Chat</p>
+        <br></br>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "40px",
+          }}
+        >
+          <Link href="/login">
+            <button style={{ fontSize: "20px" }}>Go to Login</button>
+          </Link>
+          <span style={{ fontSize: "20px" }}> or </span>
+          <Link href="/api/auth/signin">
+            <button style={{ fontSize: "20px" }}>Sign with Google</button>
+          </Link>          
+        </div>
+      </div>
     </div>
   );
 }
