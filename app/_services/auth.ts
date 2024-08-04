fe752a6243cf0e254a5356428ab6277cd41da760
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-08-04 09:54:18 +0300

    redirect auth() to /account

--- app/_services/auth.ts
+++ app/_services/auth.ts
@@ -7,7 +7,13 @@ const authConfig={
             clientId: process.env.AUTH_GOOGLE_ID,
             clientSecret: process.env.AUTH_GOOGLE_SECRET
         })
-    ]
+    ],
+    callbacks: {
+      async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
+        // Пренасочване към /account след успешна автентикация
+        return `${baseUrl}/account`
+      },
+    },
 }
 
 export const {auth, handlers:{GET, POST}} = NextAuth(authConfig);
