c0b65646b298510246c8e43f5d1bc436b3249e84
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-09-18 17:27:33 +0300

    Search in chats

--- app/_components/UserAndSearch.tsx
+++ app/_components/UserAndSearch.tsx
@@ -18,7 +18,7 @@ function UserAndSearch() {
         style={{ width: "60%", borderRadius: "7px" }}
         value={searchQuery}
         onChange={(e) => dispatch(setSearchQuery(e.target.value))}
-        placeholder=" 🔍 Search in users or groups"
+        placeholder=" 🔍 Search in chats"
       />
     </div>
   );
