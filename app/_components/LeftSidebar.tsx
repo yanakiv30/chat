b70fb43e9958e568a0eec5d01d3283aa6a99a2a9
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-10-31 10:24:39 +0200

    flex-wrap

--- app/_components/LeftSidebar.tsx
+++ app/_components/LeftSidebar.tsx
@@ -11,7 +11,7 @@ export default function LeftSidebar() {
   return (
     loggedInUser && (
       <div className="left-sidebar">
-        <p>ChatApp</p>
+        <p>ПГИТ</p>
         <Avatar name={loggedInUser.avatar} height="60px" width="60px" />
         <p style={{ width: "min-content" }}> {loggedInUser.username}</p>
         <GroupAndLogout />
